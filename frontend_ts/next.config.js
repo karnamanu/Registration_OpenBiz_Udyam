20daa11d506d3030a9e666ef07165e88cb01a60c
karnamanu <manojbm497@gmail.com>
2025-08-14 16:18:41 +0530

    Initial set up completed

---
+++ frontend_ts/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	output: "standalone",
+	// Server external packages (moved from experimental.serverComponentsExternalPackages)
+	serverExternalPackages: [],
+};
+
+module.exports = nextConfig;
